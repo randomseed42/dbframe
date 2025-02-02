847973f5c500ec24fc0ed7713710ab2937004658
randomseed42 <randomseed42@protonmail.com>
2025-02-03 05:04:53 +0800

    add logger

--- src/dbframe/sqlite_handler.py
+++ src/dbframe/sqlite_handler.py
@@ -1,7 +1,10 @@
 import os
 
-import pandas as pd
 from sqlalchemy import create_engine
+
+from .logger import Logger
+
+logger = Logger('SQLite_Logger').get_logger()
 
 
 class SQLiteHandler:
@@ -9,12 +12,21 @@ class SQLiteHandler:
         self.db_path = db_path
         self.url = self._generate_url()
         self.engine = create_engine(self.url, isolation_level='AUTOCOMMIT')
+        self._validate_connection()
 
     def _generate_url(self):
         url_template = 'sqlite:///{}'
         if self.db_path is None or self.db_path == ":memory:":
             return url_template.format(':memory:')
         return url_template.format(os.path.abspath(self.db_path))
+
+    def _validate_connection(self):
+        try:
+            self.engine.connect()
+            logger.info('Connection successful')
+        except Exception as err:
+            logger.error(str(err))
+            raise ValueError(f'Connection failed: {self.db_path=}')
 
     def select(self):
         print(f'sqlite {self.url} select')
